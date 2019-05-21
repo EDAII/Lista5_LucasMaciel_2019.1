7468b01efaa3f0e4b9bd79e4715b524cc946feda
lucas <lucasmacielaguiar@gmail.com>
2019-05-21 00:44:37 -0300

    inicio do balanceamento

--- avl.py
+++ avl.py
@@ -2,8 +2,9 @@ class BinaryElem:
     def __init__(self, value):
         self.child_left = None
         self.child_right = None
-        self.dad = None
+        self.parent = None
         self.value = value
+        self.balancing_factor = 0
 
 
 class BinaryTree:
@@ -25,30 +26,95 @@ class BinaryTree:
             else:
                 return None
 
+    def left_rotate(self, elem):
+        new_parent = elem.child_right
+        new_child = new_parent.child_left
+        new_parent.child_left = elem
+        elem.child_right = new_child
+        # diminuir fator
+        elem.balancing_factor -= 1
+        new_parent.balancing_factor -= 1
+        return new_parent
+
+    def right_rotate(self, elem):
+        new_parent = elem.child_left
+        new_child = new_parent.child_right
+        new_parent.child_right = elem
+        elem.child_left = new_child
+        # diminuir fator
+        elem.balancing_factor += 1
+        new_parent.balancing_factor += 1
+        return new_parent
+
+    def balancing(self, child, parent):
+        if child.value > parent.value:
+            parent.balancing_factor += 1
+        else:
+            parent.balancing_factor -= 1
+        if parent.balancing_factor == 0:
+            # se depois de calc o balanc == 0, entao nao vai
+            # afetar os nós anteriores
+            return
+        elif parent.balancing_factor <= -2:
+            print("valor = ", parent.value)
+            if child.balancing_factor >= 1:
+                # se o desbalanceamento estiver para dentro, realiza duas
+                # rotacoes (child e parent)
+                child = self.left_rotate(child)
+            parent = self.right_rotate(parent)
+            return
+        elif parent.balancing_factor >= 2:
+            if child.balancing_factor <= -1:
+                # se desbalanceamento estiver dentro, realiza duas
+                # rotacoes (child e parent)
+                child = self.right_rotate(child)
+            parent = self.left_rotate(parent)
+            return
+        elif parent.parent != None:
+            return self.balancing(parent, parent.parent)
+
     def insert_elem(self, elem):
         if isinstance(elem, BinaryElem):
-            dad = self.search_pos_insert(elem)
-            elem.dad = dad
-            if dad != None:
-                if elem.value > dad.value:
-                    dad.child_right = elem
+            parent = self.search_pos_insert(elem)
+            elem.parent = parent
+            if parent != None:
+                if elem.value > parent.value:
+                    parent.child_right = elem
+                    # parent = self.balancing(elem, parent)
                 else:
-                    dad.child_left = elem
+                    parent.child_left = elem
+                    parent = self.balancing(elem, parent)
             else:
+                # elem adicionado é a raiz se não houver outros elementos
                 self.root = elem
 
+    def in_order(self, elem=0):
+        if elem == 0:
+            elem = self.root
+        if elem != None:
+            self.in_order(elem.child_left)
+            print(elem.value, " h = ", elem.balancing_factor, end=", ")
+            self.in_order(elem.child_right)
 
-def insert(tree, elem):
-    return tree
+    def pre_order(self, elem=0):
+        if elem == 0:
+            elem = self.root
+        if elem != None:
+            print(elem.value, end=" ")
+            self.pre_order(elem.child_left)
+            self.pre_order(elem.child_right)
 
 
 def main():
     tree = BinaryTree()
-    array = [1, 5, 33, 20, 9, 3, 2, 56]
+    array = [20, 33, 5, 9, 3, 2]  # , 56]
     for elem in array:
         tree.insert_elem(BinaryElem(elem))
-
-    print(tree.root.child_right.value)
+    print("In Order")
+    tree.in_order()
+    print()
+    tree.pre_order()
+    print()
 
 
 main()
