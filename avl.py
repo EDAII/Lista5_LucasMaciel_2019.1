d5920093a1d9f67c63379116b32220505172453e
lucas <lucasmacielaguiar@gmail.com>
2019-05-22 11:28:34 -0300

    gambiarra para calcular altura e fator de balanceamento dos nodes

--- avl.py
+++ avl.py
@@ -1,10 +1,11 @@
 class BinaryElem:
     def __init__(self, value):
-        self.child_left = None
-        self.child_right = None
+        self.left_child = None
+        self.right_child = None
         self.parent = None
         self.value = value
-        self.balancing_factor = 0
+        self.right_height = 0
+        self.left_height = 0
 
 
 class BinaryTree:
@@ -13,62 +14,79 @@ class BinaryTree:
 
     def search_pos_insert(self, elem):
         if isinstance(elem, BinaryElem):
-            root_cp = self.root
-            aux = root_cp
-            if root_cp != None:
+            parent = self.root
+            aux = parent
+            if parent != None:
                 while (aux != None):
-                    root_cp = aux
-                    if elem.value > root_cp.value:
-                        aux = root_cp.child_right
+                    parent = aux
+                    if elem.value > parent.value:
+                        aux = parent.right_child
                     else:
-                        aux = root_cp.child_left
-                return root_cp
+                        aux = parent.left_child
+                return parent
             else:
                 return None
 
+    def fix_height(self, child, parent):
+        # Consertar alturas do filho e pai
+        l_h = child.left_child.left_height
+        r_h = child.left_child.right_height
+        # somando 1 a maior altura dos filhos depois da rotacao
+        child.left_height = 1 + (l_h if l_h >= r_h else r_h)
+        l_h = child.right_child.left_height
+        r_h = child.right_child.right_height
+        child.right_height = 1 + (l_h if l_h >= r_h else r_h)
+
+        l_h = parent.left_child.left_height
+        r_h = parent.left_child.right_height
+        parent.left_height = 1 + (l_h if l_h >= r_h else r_h)
+        l_h = parent.right_child.left_height
+        r_h = parent.right_child.right_height
+        parent.right_height = 1 + (l_h if l_h >= r_h else r_h)
+
     def left_rotate(self, elem):
-        new_parent = elem.child_right
-        new_child = new_parent.child_left
-        new_parent.child_left = elem
+        new_parent = elem.right_child
+        new_child = new_parent.left_child
+        new_parent.left_child = elem
         new_parent.parent = elem.parent
-        elem.child_right = new_child
+        elem.right_child = new_child
         elem.parent = new_parent
         return new_parent
 
     def right_rotate(self, elem):
-        new_parent = elem.child_left
-        new_child = new_parent.child_right
-        new_parent.child_right = elem
+        new_parent = elem.left_child
+        new_child = new_parent.right_child
+        new_parent.right_child = elem
         new_parent.parent = elem.parent
-        elem.child_left = new_child
+        elem.left_child = new_child
         elem.parent = new_parent
+
+        self.fix_height(elem, new_parent)
         return new_parent
 
     def balancing(self, child, parent):
         if child.value > parent.value:
-            parent.balancing_factor += 1
+            parent.right_height += 1
         else:
-            parent.balancing_factor -= 1
-        if parent.balancing_factor == 0:
-            # se depois de calc o balanc == 0, entao nao vai
-            # afetar os nós anteriores
+            parent.left_height += 1
+        if parent.right_height == parent.left_height:
+            # se as alturas forem iguais, então não vão afetar
+            # a altura dos nós anteriories
             return
-        if parent.balancing_factor <= -2:
-            if child.balancing_factor >= 1:
+        balancing_factor_p = parent.right_height - parent.left_height
+        balancing_factor_c = child.right_height - child.left_height
+        if balancing_factor_p <= -2:
+            if balancing_factor_c >= 1:
                 # se o desbalanceamento estiver para dentro, realiza duas
                 # rotacoes (child e parent)
                 child = self.left_rotate(child)
             parent = self.right_rotate(parent)
-            parent.balancing_factor += 1
-            parent.child_right.balancing_factor += 2
-        elif parent.balancing_factor >= 2:
-            if child.balancing_factor <= -1:
+        elif balancing_factor_p >= 2:
+            if balancing_factor_c <= -1:
                 # se desbalanceamento estiver dentro, realiza duas
                 # rotacoes (child e parent)
                 child = self.right_rotate(child)
             parent = self.left_rotate(parent)
-            parent.balancing_factor -= 1
-            parent.child_right.balancing_factor -= 2
         if parent.parent != None:
             self.balancing(parent, parent.parent)
         else:
@@ -82,10 +100,10 @@ class BinaryTree:
             elem.parent = parent
             if parent != None:
                 if elem.value > parent.value:
-                    parent.child_right = elem
+                    parent.right_child = elem
                     self.balancing(elem, parent)
                 else:
-                    parent.child_left = elem
+                    parent.left_child = elem
                     self.balancing(elem, parent)
             else:
                 # elem adicionado é a raiz se não houver outros elementos
@@ -95,17 +113,18 @@ class BinaryTree:
         if elem == 0:
             elem = self.root
         if elem != None:
-            self.in_order(elem.child_left)
-            print(elem.value, " = ", elem.balancing_factor, end=", ")
-            self.in_order(elem.child_right)
+            self.in_order(elem.left_child)
+            print(elem.value, " l:", elem.left_height,
+                  " r:", elem.right_height, end=", ")
+            self.in_order(elem.right_child)
 
     def pre_order(self, elem=0):
         if elem == 0:
             elem = self.root
         if elem != None:
             print(elem.value, end=" ")
-            self.pre_order(elem.child_left)
-            self.pre_order(elem.child_right)
+            self.pre_order(elem.left_child)
+            self.pre_order(elem.right_child)
 
 
 def main():
